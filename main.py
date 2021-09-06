1b6df4d8832ff36e7536248cc1e48b930cbacb45
Itsu <raining@bk.ru>
2021-09-06 18:17:22 +0300

    delete ssl protocol

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ def about():
 def contact():
     form=ContactForm()
     if request.method == 'POST':
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(user=MY_EMAIL, password=MY_PASSWORD)
             connection.sendmail(
